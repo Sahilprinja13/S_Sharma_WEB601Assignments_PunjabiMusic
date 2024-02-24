9ce9d9cddaacce45d16865b458c43e9dc9aaf8e7
Sahil Sharma <sahilprinja0183@gmail.com>
2024-02-24 14:53:59 -0500

    final commit

--- sahil sharma/src/app/app.component.ts
+++ sahil sharma/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContentCardComponent } from './content-card/content-card.component';
-import { ContentListComponent } from "./content-list/content-list.component";
+import { ContentListComponent } from './content-list/content-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ContentCardComponent, ContentListComponent],
+  imports: [RouterOutlet, ContentCardComponent, ContentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'S_Sharma_PunjabiMusic';
+  title = 'Sahil Sharma Punjabi Music';
 }
+
--- sahil sharma/src/app/helper-files/content-interface.ts
+++ sahil sharma/src/app/helper-files/content-interface.ts
@@ -1,9 +1,11 @@
-export interface Content{
+//define an interface with the following parameters
+//Note: ? is optional
+export interface Content {
     id: number;
     title: string;
     description: string;
     creator: string;
-    imgUrl?: string;
+    imgURL?: string;
     type?: string;
     tags?: string[];
 }
